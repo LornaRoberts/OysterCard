52260438892815b685c1783da3e4229e81d56e58
keowashere <michaelkeohane@live.com>
2020-10-19 15:09:08 +0100

    init balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,3 +1,8 @@
-describe "OysterCard" do
-
+require './lib/oyster_card.rb'
+describe OysterCard do
+  describe "initialize" do
+    it "starting balance of 0" do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
